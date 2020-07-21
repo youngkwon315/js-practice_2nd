2e39fbc51f9854a78bba031642676d0f6ab919f3
youngkwon315 <39914288+youngkwon315@users.noreply.github.com>
2020-07-21 22:51:53 +0900

    added todo and weather js file

---
+++ weather.js
@@ -0,0 +1,37 @@
+const COORDS = "coords";
+
+function saveCoords(coordsObj) {
+  localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSuccess(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const coordsObj = {
+    latitude,
+    longitude,
+  };
+  saveCoords(coordsObj);
+}
+
+function handleGeoError() {
+  console.log("cant handle geolocation");
+}
+
+function askForCoords() {
+  navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
+}
+
+function loadCoords() {
+  const loadedCoords = localStorage.getItem(COORDS);
+  if (loadedCoords === null) {
+    askForCoords();
+  } else {
+  }
+}
+
+function init() {
+  loadCoords();
+}
+
+init();
